8ac069bab02aea919d7e741cc953e3e653bd01e3
santiago <santiago>
2020-02-21 14:34:15 +0000

    Red y servicios terminado

--- LAB2/punto2_2.sh
+++ LAB2/punto2_2.sh
@@ -1,2 +1,3 @@
 #/bin/sh
-ps axo user,uid,size,pcpu,comm --sort -pcpu
+ps axo user,uid,size,pcpu,comm --sort -pcpu > test2.txt
+awk 'NR >=2 && NR<=10 {print "USUARIO:  " $1  "  TAMAÑO:" $3  "   USO DE CPU: " $4 "  NOMBRE PROCESO:  " $5}' test2.txt
--- LAB2/punto2_3.sh
+++ LAB2/punto2_3.sh
@@ -1,2 +1,4 @@
 #/bin/sh
-netstat -e
+
+netstat -p -o > test.txt
+awk ' NR >= 10 && NR<=20 {print "\n" "Protocolo: " $1  "  Estado Conexion: " $6 "  Nombre del programa: " $8 "  Path del programa o PID: "$9}' test.txt
